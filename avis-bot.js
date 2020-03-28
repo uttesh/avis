f354d3a023f21dcd9b629e8ee30dff03be295a7d
uttesh <uttesh@rivetsys.com>
2020-03-29 00:51:33 +0530

    added conditions

--- avis-bot.js
+++ avis-bot.js
@@ -172,26 +172,26 @@ async function scheduleTask() {
     console.log('day of the week : ',today.getDay())
     let currentTime = formatAMPM(today).split(':');
     let openingDays = [ 1, 2, 3, 4 , 5 ];
-    userIdList.forEach(userId => {
-      if (userId === 'U1FAMB9QR') {
-        let user = usersStore[userId].user;
-        sendCheck(user);
-      }
-    });
-  //   if(openingDays.includes( today.getDay() )){
-  //   if (workingTime(currentTime)) {
-  //     userIdList.forEach(userId => {
-  //       if (userId === 'U1FAMB9QR') {
-  //         let user = usersStore[userId].user;
-  //         sendCheck(user);
-  //       }
-  //     });
-  //   } else {
-  //     console.log('its not working hour !!!!');
-  //   }
-  // }else {
-  //   console.log('its Weekend !!!!');
-  // }
+    // userIdList.forEach(userId => {
+    //   if (userId === 'U1FAMB9QR') {
+    //     let user = usersStore[userId].user;
+    //     sendCheck(user);
+    //   }
+    // });
+    if(openingDays.includes( today.getDay() )){
+    if (workingTime(currentTime)) {
+      userIdList.forEach(userId => {
+        if (userId === 'U1FAMB9QR') {
+          let user = usersStore[userId].user;
+          sendCheck(user);
+        }
+      });
+    } else {
+      console.log('its not working hour !!!!');
+    }
+  }else {
+    console.log('its Weekend !!!!');
+  }
   });
 }
 
